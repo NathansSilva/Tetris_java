fab730a5749228c35f561c690255c6c09fd35558
Nathan <Nathansilva18@outlook.com>
2025-01-09 22:22:30 -0300

    Collision

--- src/main/java/main/PlayManager.java
+++ src/main/java/main/PlayManager.java
@@ -3,6 +3,7 @@ package main;
 import mino.*;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PlayManager {
@@ -24,24 +25,32 @@ public class PlayManager {
     public static Color[] colors = {Color.orange, Color.blue, Color.red, Color.cyan, Color.green, Color.magenta, Color.yellow};
     public static Random random = new Random();
     public static int randomIndex = random.nextInt(colors.length);
+    Mino nextMino;
+    final int NEXTMINO_X;
+    final int NEXTMINO_Y;
+    public static ArrayList<Block> staticBlocks = new ArrayList<>();
 
     //Others
-    public static int dropInterval = 60;
+    public static int dropInterval = 60; //mino drops in every 60 frames
 
     public PlayManager() {
         //Main Play Area Frame
-        left_x = (GamePanel.WIDTH/2) - (WIDTH/2);
-        right_x = left_x+WIDTH;
+        left_x = (GamePanel.WIDTH / 2) - (WIDTH / 2);
+        right_x = left_x + WIDTH;
         top_y = 50;
-        bottom_y = top_y+HEIGHT;
+        bottom_y = top_y + HEIGHT;
 
-        MINO_START_X = left_x + (WIDTH/2) - Block.SIZE;
+        MINO_START_X = left_x + (WIDTH / 2) - Block.SIZE;
         MINO_START_Y = top_y + Block.SIZE;
+
+        NEXTMINO_X = right_x + 175;
+        NEXTMINO_Y = top_y + 500;
 
         //Starting Mino
         currentMino = pickMino();
         currentMino.setXY(MINO_START_X, MINO_START_Y);
-
+        nextMino = pickMino();
+        nextMino.setXY(NEXTMINO_X, NEXTMINO_Y);
     }
 
     private Mino pickMino() {
@@ -64,7 +73,22 @@ public class PlayManager {
 
 
     public void update() {
-        currentMino.update();
+        if (!currentMino.active) {
+            //if not active put it in the array
+            staticBlocks.add(currentMino.b[0]);
+            staticBlocks.add(currentMino.b[1]);
+            staticBlocks.add(currentMino.b[2]);
+            staticBlocks.add(currentMino.b[3]);
+
+            //nextMino
+            currentMino = nextMino;
+            currentMino.setXY(MINO_START_X, MINO_START_Y);
+            nextMino = pickMino();
+            nextMino.setXY(NEXTMINO_X, NEXTMINO_Y);
+
+        } else {
+            currentMino.update();
+        }
     }
 
     public void draw(Graphics2D g2) {
@@ -72,7 +96,7 @@ public class PlayManager {
         //Playing Area
         g2.setColor(Color.WHITE);
         g2.setStroke(new BasicStroke(4f));
-        g2.drawRect(left_x-4, top_y-4, WIDTH+8, HEIGHT+8);
+        g2.drawRect(left_x - 4, top_y - 4, WIDTH + 8, HEIGHT + 8);
 
         //Next Mino Frame
         int x = right_x + 100;
@@ -80,11 +104,27 @@ public class PlayManager {
         g2.drawRect(x, y, 200, 200);
         g2.setFont(new Font("Arial", Font.PLAIN, 30));
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g2.drawString("NEXT", x+60, y+60);
+        g2.drawString("NEXT", x + 60, y + 60);
 
         //Draw the currentMino
         if (currentMino != null) {
             currentMino.draw(g2);
+        }
+
+        //Draw the nextMino
+        nextMino.draw(g2);
+
+        //Static blocks draw
+        for (int i = 0; i < staticBlocks.size(); i++) {
+            staticBlocks.get(i).draw(g2);
+        }
+
+        g2.setColor(Color.YELLOW);
+        g2.setFont(g2.getFont().deriveFont(50f));
+        if (MovementHandler.pausePressed) {
+            x = left_x + 90;
+            y = top_y + 310;
+            g2.drawString("PAUSE", x, y);
         }
 
     }
